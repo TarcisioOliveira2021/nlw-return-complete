cad9cf4420edcf86cc76971b229a34fee86afd74
TarcisioOliveira2021 <tarcisio.zark.veloso@gmail.com>
2022-05-14 17:29:23 -0300

    Ajustes no deploy do backend conexão com o bd.

--- server/server/src/routers.ts
+++ server/server/src/routers.ts
@@ -9,16 +9,22 @@ routes.post('/feedbacks', async (req, res) => {
     //Permite a chamada sem especificar dentro do prisma.feedbacks.create 
     const { type, comment, screenshot } = req.body;
 
-    const nodemailerMailAdapter = new NodemailerMailAdapter();
-    const prismaFeedbackRepository = new PrismaFeedbacksRepository();
-    const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedbackRepository, nodemailerMailAdapter)
+    try {
+        const nodemailerMailAdapter = new NodemailerMailAdapter();
+        const prismaFeedbackRepository = new PrismaFeedbacksRepository();
+        const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedbackRepository, nodemailerMailAdapter)
 
 
-    await submitFeedbackUseCase.execute({
-        type,
-        comment,
-        screenshot,
-    })
+        await submitFeedbackUseCase.execute({
+            type,
+            comment,
+            screenshot,
+        })
 
-    return res.status(201).send();
+        return res.status(201).send();
+
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send();
+    }
 })
